acbc4316fbf66a1ddfeb6d5e13777d55a9d339d8
tomWindwos <fichtner.dk@hotmail.com>
2022-04-06 08:22:09 -0300

    #4 Created controller and first code

---
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -0,0 +1,21 @@
+package com.api.parkingcontrol.controllers;
+
+import com.api.parkingcontrol.services.ParkinSpotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*",maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+    final ParkinSpotService parkinSpotService;
+
+    public ParkingSpotController(ParkinSpotService parkinSpotService) {
+        this.parkinSpotService = parkinSpotService;
+    }
+
+
+
+}
