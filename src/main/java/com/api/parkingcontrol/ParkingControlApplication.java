a398a2509741631ff5190aa2bb9d9fdc3ff90bd3
tomWindwos <fichtner.dk@hotmail.com>
2022-04-09 01:00:39 -0300

    #8 Refactored Code

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -13,9 +13,4 @@ public class ParkingControlApplication {
     public static void main(String[] args) {
         SpringApplication.run(ParkingControlApplication.class, args);
     }
-    @GetMapping("/")
-public String index(){
-    return "Hello World";
-}
-
 }
--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -1,21 +1,35 @@
 package com.api.parkingcontrol.controllers;
 
-import com.api.parkingcontrol.services.ParkinSpotService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.parkingcontrol.dto.ParkingSpotDto;
+import com.api.parkingcontrol.exeption.DataIntegrityViolationException;
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.services.ParkingSpotService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*",maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/parking-spot")
 public class ParkingSpotController {
-    final ParkinSpotService parkinSpotService;
 
-    public ParkingSpotController(ParkinSpotService parkinSpotService) {
-        this.parkinSpotService = parkinSpotService;
+    final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
     }
 
+    @PostMapping
+    public ResponseEntity<ParkingSpotModel> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) throws DataIntegrityViolationException {
+        return parkingSpotService.save(parkingSpotDto);
+    }
 
+    @GetMapping
+    public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpots(){
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+    }
 
 }
